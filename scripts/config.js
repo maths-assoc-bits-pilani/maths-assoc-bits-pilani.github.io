fe7751d421f22abc64f1e7ab65ddb09143192e3e
asli-nobita <hamzanajfi2765@gmail.com>
2026-01-07 10:58:31 +0530

    changed API_BASE

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,2 @@
-const API_BASE = "http://localhost:5000"; // Update for production
-// const API_BASE = "https://math-assoc-api.onrender.com";
+// const API_BASE = "http://localhost:5000"; // Update for production
+const API_BASE = "https://math-assoc-api.onrender.com";
